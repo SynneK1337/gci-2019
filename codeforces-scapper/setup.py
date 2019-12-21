b1da261f2e55bf1cf9c121d6563eca7a7b9149b9
SynneK1337 <synnek1337@protonmail.com>
2019-12-21 23:37:15 +0100

    Add setup.py

---
+++ codeforces-scapper/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="codeforces-wrapper",
+	version="1.0",
+	description="Codeforces API Wrapper",
+	url="https://github.com/synnek1337/gci-2019",
+	author="Emilian Zawrotny",
+	author_email="synnek1337@protonmail.com",
+	packages=find_packages()
+)
