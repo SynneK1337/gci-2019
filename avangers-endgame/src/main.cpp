bc3a57a79c42a28510ba2b0a81b91b62a00caeda
SynneK1337 <synnek1337@protonmail.com>
2020-01-17 13:05:02 +0100

    Split app into single file

--- avangers-endgame/src/main.cpp
+++ avangers-endgame/src/main.cpp
@@ -1,6 +1,29 @@
-#include "Thanos.h"
+#include <string>
 #include <iostream>
 #include <vector>
+#include <cmath>
+
+class Thanos {
+public:
+	float hp;
+	std::string killer;
+	unsigned long long days_needed_to_kill = 0;
+	Thanos(float hp) {
+		this->hp = hp;
+		if (this->hp > 1) {
+			// hp - 1 because they deal for D+1 on the last day
+			// Modern Problems Requires Modern Solutions
+			this->days_needed_to_kill = std::ceil(-0.5 + std::sqrt((0.25 + 2 * (this->hp - 1))));
+		}
+		else {
+			this->days_needed_to_kill = std::ceil(-0.5 + std::sqrt((0.25 + 2 * this->hp)));
+		}
+		
+		this->killer = (days_needed_to_kill % 2 == 0) ? "Captain America" : "Iron man";
+	}
+};
+
+
 
 int main() {
 	std::vector<float> test_checks{};
